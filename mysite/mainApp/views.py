e22e9285a547049845261ffec76a1730886e6584
AntonUkhov <89222229263@mail.ru>
2021-08-14 10:23:09 +0500

    added contact page

--- mysite/mainApp/views.py
+++ mysite/mainApp/views.py
@@ -5,4 +5,4 @@ def index(request):
     return render(request, 'mainApp/homePage.html')
 
 def contact(request):
-    return render(request, 'mainApp/contact.html')
+    return render(request, 'mainApp/basic.html', {'values': ['Если у вас остались вопросы, то задавайте их мне по телефону', '8 999 999 99 99', '9846@auto.ru']})
